de5f14a8764571eca54f935422ec2ab1277a10bf
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-05-07 12:16:27 +0530

    initial setup, added dependencies

---
+++ ktor-server/src/main/kotlin/org/yellowhatpro/Application.kt
@@ -0,0 +1,15 @@
+package org.yellowhatpro
+
+import io.ktor.server.application.*
+import org.yellowhatpro.plugins.*
+
+fun main(args: Array<String>): Unit =
+        io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+    configureSecurity()
+    configureMonitoring()
+    configureSerialization()
+}
